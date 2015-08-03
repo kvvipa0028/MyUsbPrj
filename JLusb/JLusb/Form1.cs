cd01c047ad4468477b64a9bdd61759827959d6cb
Ham <secretan@163.com>
2015-08-03 09:50:25 +0800

    增加一个工程，用于开发测试USB模块
    Signed-off-by: Ham <secretan@163.com>

---
+++ JLusb/JLusb/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JLusb
+{
+    public partial class Form1 : Form
+    {
+        JLusb jlusb = new JLusb();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            jlusb.get_video();
+        }
+    }
+}
---
+++ JLusb/JLusb/JLusb.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using CyUSB;
+
+namespace JLusb
+{
+    public class JLusb
+    {
+        private USBDeviceList mUSBDevList;
+        private CyUSBDevice mCyUSBDev;
+        private CyUSBEndPoint mCyUSBInEP;
+        private CyUSBEndPoint mCyUSBOutEP;
+        private byte[] mFrame = new byte[512];
+
+        public JLusb()
+        {
+            mUSBDevList = new USBDeviceList(CyConst.DEVICES_CYUSB);
+            mUSBDevList.DeviceAttached += new EventHandler(mUSBDevList_Attached);
+            mUSBDevList.DeviceRemoved += new EventHandler(mUSBDevList_Removed);
+            if (mUSBDevList.Count > 0)
+            {
+                USBDetected();
+            }
+        } // end public JLusb()
+        /// <summary>
+        /// detected the usb is attaching
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mUSBDevList_Attached(Object sender, EventArgs e)
+        {
+            USBDetected();
+        } // end private void mUSBDevList_Attached(Object sender, EventArgs e)
+        /// <summary>
+        /// detected the usb is removing
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mUSBDevList_Removed(Object sender, EventArgs e)
+        {
+            mCyUSBDev.Dispose();
+            mUSBDevList.Dispose();
+            mUSBDevList.DeviceAttached -= mUSBDevList_Attached;
+            mUSBDevList.DeviceRemoved -= mUSBDevList_Removed;
+        } // end private void mUSBDevList_Removed(Object sender, EventArgs e)
+        /// <summary>
+        /// function for all detected thing
+        /// </summary>
+        private void USBDetected()
+        {
+            mCyUSBDev = mUSBDevList[0x04b4, 0x1004] as CyUSBDevice;
+            if (mCyUSBDev == null)
+            {
+                MessageBox.Show("No Such Device!");
+            }
+            else
+            {
+                mCyUSBInEP = mCyUSBDev.BulkInEndPt;
+                mCyUSBOutEP = mCyUSBDev.BulkOutEndPt;
+            }
+        } //private void USBDetected()
+        public bool get_video()
+        {
+            int len = 512;
+            bool received = false;
+            received = mCyUSBInEP.XferData(ref mFrame, ref len);
+            return received;
+        }
+    } // end public class JLusb
+
+} // end namespace JLusb
